2acce1c5b9ba307a6a86f68c936486d458416aa4
Pranav Bheda <pranav.bheda.dev@gmail.com>
2016-10-19 13:18:51 -0700

    first version of courses

---
+++ courses.js
@@ -0,0 +1,34 @@
+{"course":[
+    {"department":"COEN", "courseNumber":"010"},
+    {"department":"COEN", "courseNumber":"011"},
+    {"department":"COEN", "courseNumber":"012"},
+    {"department":"COEN", "courseNumber":"019"},
+    {"department":"COEN", "courseNumber":"020"},
+    {"department":"COEN", "courseNumber":"021"},
+    {"department":"COEN", "courseNumber":"070"},
+    {"department":"COEN", "courseNumber":"122"},
+    {"department":"COEN", "courseNumber":"146"},
+    {"department":"COEN", "courseNumber":"171"},
+    {"department":"COEN", "courseNumber":"174"},
+    {"department":"COEN", "courseNumber":"175"},
+    {"department":"COEN", "courseNumber":"179"},
+    
+    {"department":"ENGR", "courseNumber":"001"},
+
+    {"department":"MATH", "courseNumber":"011"},
+    {"department":"MATH", "courseNumber":"012"},
+    {"department":"MATH", "courseNumber":"013"},
+    {"department":"MATH", "courseNumber":"014"},
+    {"department":"MATH", "courseNumber":"053"},
+
+    {"department":"AMTH", "courseNumber":"106"},
+    {"department":"AMTH", "courseNumber":"108"},
+
+    {"department":"PHYS", "courseNumber":"031"},
+    {"department":"PHYS", "courseNumber":"032"},
+    {"department":"PHYS", "courseNumber":"033"},
+    {"department":"CHEM", "courseNumber":"011"},
+
+    {"department":"ELEN", "courseNumber":"050"},
+    {"department":"ELEN", "courseNumber":"153"},
+]}
