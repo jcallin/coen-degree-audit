fee844bcb3fe68db310092ec6ac0fc900375169b
Pranav Bheda <pranav.bheda.dev@gmail.com>
2016-10-21 13:29:58 -0700

    updated course list with my classes taken

--- courses.js
+++ courses.js
@@ -1,34 +1,103 @@
-{"course":[
-    {"department":"COEN", "courseNumber":"010"},
-    {"department":"COEN", "courseNumber":"011"},
-    {"department":"COEN", "courseNumber":"012"},
-    {"department":"COEN", "courseNumber":"019"},
-    {"department":"COEN", "courseNumber":"020"},
-    {"department":"COEN", "courseNumber":"021"},
-    {"department":"COEN", "courseNumber":"070"},
-    {"department":"COEN", "courseNumber":"122"},
-    {"department":"COEN", "courseNumber":"146"},
-    {"department":"COEN", "courseNumber":"171"},
-    {"department":"COEN", "courseNumber":"174"},
-    {"department":"COEN", "courseNumber":"175"},
-    {"department":"COEN", "courseNumber":"179"},
+{"courses": [
+
+    // COEN Major
+    // -----------------------
+    {"course": "COEN_010"},
+    {"course": "COEN_011"},
+    {"course": "COEN_012"},
+    {"course": "COEN_020"},
+    {"course": "COEN_021"},
+    {"course": "COEN_070"},
+    {"course": "COEN_122"},
+    {"course": "COEN_146"},
+    {"course": "COEN_171"},
+    {"course": "COEN_174"},
+    {"course": "COEN_175"},
+    {"course": "COEN_177"},
+    {"course": "COEN_179"},
+    {"course": "COEN_194"},
+    {"course": "COEN_195"},
+    {"course": "COEN_196"},
     
-    {"department":"ENGR", "courseNumber":"001"},
+    {"course": "ENGR_001"},
 
-    {"department":"MATH", "courseNumber":"011"},
-    {"department":"MATH", "courseNumber":"012"},
-    {"department":"MATH", "courseNumber":"013"},
-    {"department":"MATH", "courseNumber":"014"},
-    {"department":"MATH", "courseNumber":"053"},
+    {"course": "MATH_011"},
+    {"course": "MATH_012"},
+    {"course": "MATH_013"},
+    {"course": "MATH_014"},
+    {"course": "MATH_053"},
 
-    {"department":"AMTH", "courseNumber":"106"},
-    {"department":"AMTH", "courseNumber":"108"},
+    {"course": "PHYS_031"},
+    {"course": "PHYS_032"},
+    {"course": "PHYS_033"},
+    {"course": "CHEM_011"},
 
-    {"department":"PHYS", "courseNumber":"031"},
-    {"department":"PHYS", "courseNumber":"032"},
-    {"department":"PHYS", "courseNumber":"033"},
-    {"department":"CHEM", "courseNumber":"011"},
+    {"course": "ELEN_050"},
+    {"course": "ELEN_153"},
+    // -----------------------
 
-    {"department":"ELEN", "courseNumber":"050"},
-    {"department":"ELEN", "courseNumber":"153"},
+
+    // Cross Listings
+    // -----------------------
+    {"course": "COEN_019"},
+    {"course": "MATH_051"},
+
+    {"course": "AMTH_106"},
+    {"course": "MATH_022"},
+
+    {"course": "AMTH_108"},
+    {"course": "MATH_122"},
+    // -----------------------
+
+    // Advance Math
+    // -----------------------
+    {"course": "MATH_053"},
+    {"course": "MATH_166"},
+    {"course": "MATH_118"},
+    // -----------------------
+
+
+    // CTWs
+    // -----------------------
+    {"course": "ENGL_001A"},
+    {"course": "ENGL_001B"},
+    // -----------------------
+
+    // Advance Writing
+    {"course": "ENGL_181"},
+
+    // CNI 123
+    {"course": "ENGL_011A"}, // CNI 1
+    {"course": "ENGL_011B"}, // CNI 2
+    {"course": "HIST_091"},  // CNI 3
+
+    // RTC 123
+    {"course": "TESP_004"},
+    {"course": "SCTR_065"},
+    {"course": "TESP_121"},
+
+    // ETHICS
+    {"course": "ENGR_019"},
+
+    // Diversity
+    {"course": "SOCI_033"},
+
+    // Social Science
+    {"course": "ECON_001"},
+   
+    // ELSJ
+    {"course": "ENGR_110"},
+
+
+
+    // CNI3 and Social Science 
+    {"course": "POLY_002"}
+
+    // Covered by Senior Design
+    // Language
+    // MATH
+    // Civic Engagement
+    // Arts
+    // STS
+    // Natural Science
 ]}
