6cf17be5f1bad6a5d1944dd7b196db4b537a8333
“Carlos <carlos.rivera.ece@gmail.com>
2016-10-27 12:21:57 -0700

    map from class to requirement

---
+++ requirements.js
@@ -0,0 +1,88 @@
+var requirements = {
+    // Add courses that fulfill requirement to respective array
+    // key: course
+    // value: requirement fulfilled
+    
+    // COEN Major Requirements
+    "ENGR_001":["ENGR_001"],
+    "COEN_010":["COEN_010"],
+    "COEN_011":["COEN_011"],
+    "COEN_012":["COEN_012"],
+    
+    "COEN_020":["COEN_020"],
+    "COEN_021":["COEN_021"],
+    "COEN_070":["COEN_070"],
+    "COEN_122":["COEN_122"],
+    "COEN_146":["COEN_146"],
+    "COEN_171":["COEN_171"],
+    "COEN_174":["COEN_174"],
+    "COEN_175":["COEN_175"],
+    "COEN_177":["COEN_177"],
+    "COEN_179":["COEN_179"],
+    "COEN_194":["COEN_194"],
+    "COEN_195":["COEN_195"],
+    "COEN_196":["COEN_196"],
+    
+    "MATH_011":["MATH_011"],
+    "MATH_012":["MATH_012"],
+    "MATH_013":["MATH_013"],
+    "MATH_014":["MATH_014"],
+    "MATH_053":["MATH_053"],
+    
+    "PHYS_031":["PHYS_031"],
+    "PHYS_032":["PHYS_032"],
+    "PHYS_033":["PHYS_033"],
+    "CHEM_011":["CHEM_011"],
+    
+    "ELEN_050":["ELEN_050"],
+    "ELEN_153":["ELEN_153"],
+    
+    // cross listings
+    "COEN_019":["COEN_019"],
+    "MATH_051":["COEN_019"],
+    
+    "AMTH_106":["AMTH_106"],
+    "MATH_022":["AMTH_106"],
+    
+    "AMTH_108":["AMTH_108"],
+    "MATH_122":["AMTH_108"],
+    
+    // advanced math
+    "MATH_053":["MATH_053"],
+    "MATH_166":["MATH_053"],
+    "MATH_118":["MATH_053"],
+    
+    // ctw
+    "ENGL_001A":["CTW1"],
+    "ENGL_001A":["CTW2"],
+    
+    // advanced writing
+    "ENGL_181":["ADVW"],
+    
+    // c and i
+    "ENGL_011A":["CNI1"],
+    "ENGL_011B":["CNI2"],
+    "HIST_091":["CNI3"],
+    
+    // rtc
+    "TESP_004":["RTC1"],
+    "SCTR_065":["RTC2"],
+    "TESP_121":["RTC3"],
+    
+    // ethics
+    "ENGR_019":["ETHC"],
+    
+    // diversity
+    "SOCI_033":["DVSY"],
+    
+    // social science
+    "ECON_001":["SOSC"],
+    
+    // elsj
+    "ENGR_110":["ELSJ"],
+    
+    // double dip classes
+    // CNI 3 and Social Science
+    "POLY_002":["CNI3","SOSC"]
+
+};
