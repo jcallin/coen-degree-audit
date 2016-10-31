5d361cade794c7bbdde8e6e211a3ad4307e39473
Pranav Bheda <pranav.bheda.dev@gmail.com>
2016-10-31 15:04:50 -0700

    remove grid and make list of classes

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-var data = {
-    "1_fall":[],
-    "1_winter":[],
-    "1_spring":[],
-    "1_summer":[],
-    "2_fall":[],
-    "2_winter":[],
-    "2_spring":[],
-    "2_summer":[],
-    "3_fall":[],
-    "3_winter":[],
-    "3_spring":[],
-    "3_summer":[],
-    "4_fall":[],
-    "4_winter":[],
-    "4_spring":[],
-    "4_summer":[]
-}
-
-var quarterArray = [
-    '#1_fall', '#1_winter', '#1_spring', '#1_summer',
-    '#2_fall', '#2_winter', '#2_spring', '#2_summer',
-    '#3_fall', '#3_winter', '#3_spring', '#3_summer',
-    '#4_fall', '#4_winter', '#4_spring', '#4_summer'
-];
+var data = [];
 
 var course = [
-    "COEN_010", "COEN_011", "COEN_012", "COEN_020", "COEN_021", "COEN_070", 
-    "COEN_122", "COEN_146", "COEN_171", "COEN_174", "COEN_175", "COEN_177",  
-    "COEN_179", "COEN_194", "COEN_195", "COEN_196", "ENGR_001", "MATH_011", 
-    "MATH_012", "MATH_013", "MATH_014", "MATH_053", "PHYS_031", "PHYS_032", 
-    "PHYS_033", "CHEM_011", "ELEN_050", "ELEN_153", "COEN_019", "MATH_051",
-    "AMTH_106", "MATH_022", "AMTH_108", "MATH_122", "MATH_053", "MATH_166", 
-    "HIST_091", "TESP_004", "SCTR_065", "TESP_121", "EBGR_019", "SOCI_033", 
-    "ECON_001", "ENGR_110", "POLY_002", "MATH_118", "ENGL_181", 
-    "ENGL_001A", "ENGL_001B", 
-    "ENGL_011A", "ENGL_011B" 
+    "COEN_010", "COEN_011", "COEN_012", "COEN_020", "COEN_021", "COEN_070", "COEN_122", "COEN_146", 
+    "COEN_171", "COEN_174", "COEN_175", "COEN_177", "COEN_179", "COEN_194", "COEN_195", "COEN_196", 
+    "ENGR_001", "MATH_011", "MATH_012", "MATH_013", "MATH_014", "MATH_053", "PHYS_031", "PHYS_032", 
+    "PHYS_033", "CHEM_011", "ELEN_050", "ELEN_153", "COEN_019", "MATH_051", "AMTH_106", "MATH_022", 
+    "AMTH_108", "MATH_122", "MATH_053", "MATH_166", "HIST_091", "TESP_004", "SCTR_065", "TESP_121", 
+    "EBGR_019", "SOCI_033", "ECON_001", "ENGR_110", "POLY_002", "MATH_118", "ENGL_181", "COEN_161", 
+    "COEN_162", "COEN_163", "ENGL_001A", "ENGL_001B", "ENGL_011A", "ENGL_011B" 
 ];
 
 var requirements = {
@@ -58,7 +31,12 @@ var requirements = {
     "COEN_175":["COEN_175"],
     "COEN_177":["COEN_177"],
     "COEN_179":["COEN_179"],
-    "COEN_194":["COEN_194"],
+    "COEN_194":["COEN_194", 
+                "STSS", 
+                "ARTS",
+                "CVEN",
+                "LANG",
+                "MATH"],
     "COEN_195":["COEN_195"],
     "COEN_196":["COEN_196"],
     
@@ -122,99 +100,93 @@ var requirements = {
     
     // double dip classes
     // CNI 3 and Social Science
-    "POLY_002":["CNI3","SOSC"]
+    "POLY_002":["CNI3","SOSC"],
+
+    "COEN_161":["TCH_ELE1"],
+    "COEN_162":["TCH_ELE2"],
+    "COEN_163":["TCH_ELE3"]
 };
 
 
-$(document).ready(function() {
-    $("#year").select2()  
-    $("#quarter").select2()     
+$(document).ready(function() { 
     $("#courseEntry").select2({data:course})
 });
 
 function add(){
-    var y = $("#year").select2().val();
-    var q = $("#quarter").select2().val();
     var c = $("#courseEntry").select2().val();
-    var code = y + "_" + q;
-    var obj = { year:y,     quarter:q,  courseClass:c}
-    alert(JSON.stringify(obj, null, 4));
-    data[code].push(c);
-    document.getElementById(code).innerHTML += 
-    // '<div title="' + code + '" ' +  'onClick="removeCourse(this)">' + c + '</div>' + "\n";
-    '<div data-quarter="' + code + '" data-courseid="' + c + '" onClick="removeCourse(this)">' + c + '</div>' + "\n";
-
-    checkRequirements(c);
+    data.push(c);
+    document.getElementById("list").innerHTML += '<li id="data_' + c + '" onclick="removeCourse(this)">' + c + '</li>' + "\n";
+    checkRequirements();
 }
+
+
   
 function removeCourse(obj){
-    var quarter = obj.getAttribute("data-quarter");
-    var courseToBeDeleted = obj.getAttribute("data-courseid");
+    var temp = obj.id;
+    var id = temp.substring(5,temp.length);
+    alert(id);
     var index = -1;
-
-    for(var i = 0; i < data[quarter].length; i++){
-        // alert(data[id][i]);
-        if(courseToBeDeleted == data[quarter][i]){
+    for(var i = 0; i < data.length; i++){
+        if(id == data[i]){
             index = i;
         }
     }
     if(index != -1) {
-        data[quarter].splice(index, 1);
+        data.splice(index, 1);
     }
+    removeRequirement(id);
     $(obj).remove();
-
-    $.each(requirements[courseToBeDeleted], function(index,value) {
-        setToRed(value);
-    })
+    checkRequirements();
 }
 
-
-// $(document).ready(function() {
-//     for(var i = 0; i < course.length; i++) {
-//         document.getElementById(course[i]).onclick = setToGreen();   
-//     }
-// });
-
-// check classes in one quarter
-function checkQuarter(quarter) {
-    var courseArray = [];
-    $(quarter + ' :selected').each(function(i, selected){
-        courseArray[i] = $(selected).text();
-    });
-    for(i = 0; i < courseArray.length; i++){
-        console.log(courseArray[i]);
-        checkRequirements(courseArray[i]);
-    }
-}
 
 // check requirements for one course
-function checkRequirements(course) {
-    $.each(requirements[course], function(index,value) {
-        setToGreen(value);
+function checkRequirements(){
+    for(var i = 0; i < data.length; i++){
+        $.each(requirements[data[i]], function(index,value) {
+            setGreen(value);
+        })
+    }
+}
+
+function removeRequirement(obj){
+    $.each(requirements[obj], function(index,value) {
+        setRed(value);
     })
 }
 
-// update course list status on click
-$(document).ready(function() {
-    $("#submit").click(function(){
-        checkAllReqs();
-    });
-});
-
-
-function checkAllReqs(){
-    $.each(quarterArray, function(index, value) {
-        checkQuarter(value);
-    })
-}
-
-
-function setToGreen(value) {
+function setGreen(value) {
     document.getElementById(value).classList.remove('btn-danger');
     document.getElementById(value).classList.add('btn-success');
 }
 
-function setToRed(value) {
+function setRed(value) {
     document.getElementById(value).classList.remove('btn-success');
     document.getElementById(value).classList.add('btn-danger');
-} 
+}
+
+function storeData(){
+    document.cookie = JSON.stringify(data);
+}
+
+function loadData(){
+    var cookieData = $.parseJSON(document.cookie);
+    alert(cookieData);
+    for(var i = 0; i < cookieData.length; i++){
+        data.push(c);
+        document.getElementById("list").innerHTML += '<li id="data_' + c + '" onclick="removeCourse(this)">' + c + '</li>' + "\n";
+    }
+    checkRequirements();
+}
+
+
+$(document).ready(function() {
+    loadData();
+});
+
+
+
+
+
+
+
