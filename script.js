2cc25dc19c0d7f58794cb2a5821e895b7060a000
Pranav Bheda <pranav.bheda.dev@gmail.com>
2016-11-01 14:07:23 -0700

    new branch with cleaned up code and ui

--- script.js
+++ script.js
@@ -1,112 +1,3 @@
-var data = [];
-
-var course = [
-    "COEN_010", "COEN_011", "COEN_012", "COEN_020", "COEN_021", "COEN_070", "COEN_122", "COEN_146", 
-    "COEN_171", "COEN_174", "COEN_175", "COEN_177", "COEN_179", "COEN_194", "COEN_195", "COEN_196", 
-    "ENGR_001", "MATH_011", "MATH_012", "MATH_013", "MATH_014", "MATH_053", "PHYS_031", "PHYS_032", 
-    "PHYS_033", "CHEM_011", "ELEN_050", "ELEN_153", "COEN_019", "MATH_051", "AMTH_106", "MATH_022", 
-    "AMTH_108", "MATH_122", "MATH_053", "MATH_166", "HIST_091", "TESP_004", "SCTR_065", "TESP_121", 
-    "EBGR_019", "SOCI_033", "ECON_001", "ENGR_110", "POLY_002", "MATH_118", "ENGL_181", "COEN_161", 
-    "COEN_162", "COEN_163", "ENGL_001A", "ENGL_001B", "ENGL_011A", "ENGL_011B" 
-];
-
-var requirements = {
-    // Add courses that fulfill requirement to respective array
-    // key: course
-    // value: requirement fulfilled
-    
-    // COEN Major Requirements
-    "ENGR_001":["ENGR_001"],
-    "COEN_010":["COEN_010"],
-    "COEN_011":["COEN_011"],
-    "COEN_012":["COEN_012"],
-    
-    "COEN_020":["COEN_020"],
-    "COEN_021":["COEN_021"],
-    "COEN_070":["COEN_070"],
-    "COEN_122":["COEN_122"],
-    "COEN_146":["COEN_146"],
-    "COEN_171":["COEN_171"],
-    "COEN_174":["COEN_174"],
-    "COEN_175":["COEN_175"],
-    "COEN_177":["COEN_177"],
-    "COEN_179":["COEN_179"],
-    "COEN_194":["COEN_194", 
-                "STSS", 
-                "ARTS",
-                "CVEG",
-                "LANG",
-                "MATH"],
-    "COEN_195":["COEN_195"],
-    "COEN_196":["COEN_196"],
-    
-    "MATH_011":["MATH_011"],
-    "MATH_012":["MATH_012"],
-    "MATH_013":["MATH_013"],
-    "MATH_014":["MATH_014"],
-    "MATH_053":["MATH_053"],
-    
-    "PHYS_031":["PHYS_031"],
-    "PHYS_032":["PHYS_032"],
-    "PHYS_033":["PHYS_033"],
-    "CHEM_011":["CHEM_011"],
-    
-    "ELEN_050":["ELEN_050"],
-    "ELEN_153":["ELEN_153"],
-    
-    // cross listings
-    "COEN_019":["COEN_019"],
-    "MATH_051":["COEN_019"],
-    
-    "AMTH_106":["AMTH_106"],
-    "MATH_022":["AMTH_106"],
-    
-    "AMTH_108":["AMTH_108"],
-    "MATH_122":["AMTH_108"],
-    
-    // advanced math
-    "MATH_053":["MATH_053"],
-    "MATH_166":["MATH_053"],
-    "MATH_118":["MATH_053"],
-    
-    // ctw
-    "ENGL_001A":["CTW1"],
-    "ENGL_001A":["CTW2"],
-    
-    // advanced writing
-    "ENGL_181":["ADVW"],
-    
-    // c and i
-    "ENGL_011A":["CNI1"],
-    "ENGL_011B":["CNI2"],
-    "HIST_091":["CNI3"],
-    
-    // rtc
-    "TESP_004":["RTC1"],
-    "SCTR_065":["RTC2"],
-    "TESP_121":["RTC3"],
-    
-    // ethics
-    "ENGR_019":["ETHC"],
-    
-    // diversity
-    "SOCI_033":["DVSY"],
-    
-    // social science
-    "ECON_001":["SOSC"],
-    
-    // elsj
-    "ENGR_110":["ELSJ"],
-    
-    // double dip classes
-    // CNI 3 and Social Science
-    "POLY_002":["CNI3","SOSC"],
-
-    "COEN_161":["TCH_ELE1"],
-    "COEN_162":["TCH_ELE2"],
-    "COEN_163":["TCH_ELE3"]
-};
-
 // Always running
 $(document).ready(function() { 
     $("#courseEntry").select2({data:course})
@@ -114,10 +5,16 @@ $(document).ready(function() {
 
 // Add a new class to the data array and change the color of the cell 
 function add(){
-    var c = $("#courseEntry").select2().val();
-    data.push(c);
-    document.getElementById("list").innerHTML += '<li id="data_' + c + '" onclick="removeCourse(this)">' + c + '</li>' + "\n";
+    var courseEntry = $("#courseEntry").select2().val();
+    data.push(courseEntry);
+    addNewListElement(courseEntry);
     checkRequirements();
+}
+
+// Adds courseEntry to list of taken classes
+function addNewListElement(courseEntry){
+    document.getElementById("list").innerHTML += 
+    '<li id="data_' + courseEntry + '" onclick="removeCourse(this)">' + courseEntry + '</li>' + "\n";
 }
 
 // When a course (by id) is removed, it is removed from the data 
@@ -176,6 +73,8 @@ function setRed(value) {
     document.getElementById(value).classList.add('btn-danger');
 }
 
+
+// Click the required cell to toggle the color between red and green
 function toggleColor(a){
     var id = a.id;
     if(document.getElementById(id).classList.contains('btn-success')){
@@ -185,26 +84,28 @@ function toggleColor(a){
     }
 }
 
-var createCookie = function(name, value, days) {
+
+// Creates Cookie
+var createCookie = function(name, value, days){
     var expires;
-    if (days) {
+    if(days){
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toGMTString();
     }
-    else {
+    else{
         expires = "";
     }
     document.cookie = name + "=" + value + expires + "; path=/";
 }
-
-function getCookie(c_name) {
-    if (document.cookie.length > 0) {
+// Gets Cookie
+function getCookie(c_name){
+    if(document.cookie.length > 0) {
         c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
+        if(c_start != -1){
             c_start = c_start + c_name.length + 1;
             c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) {
+            if(c_end == -1){
                 c_end = document.cookie.length;
             }
             return unescape(document.cookie.substring(c_start, c_end));
@@ -213,22 +114,22 @@ function getCookie(c_name) {
     return "";
 }
 
-
+// Writes the contents of the data array to a cookie
 function storeData(){
     var json_str = JSON.stringify(data);
-    var json = "'" + json_str + "'"
-    createCookie('degree_cookie', json);
+    var str = "'" + json_str + "'"
+    createCookie('degree_cookie', str);
 }
 
+// Loads the array of taken classes from the cookie and 
+// stores the taken-classes into the data array
 function loadData(){
     var cookieString =  getCookie('degree_cookie');
-    // cookieString = cookieString.substring(1, cookieString.length -1);
     for(var i = 3; i < cookieString.length - 1;){
-        var c = cookieString.substring(i, i + 8);
-        data.push(c);
+        var courseEntry = cookieString.substring(i, i + 8);
+        data.push(courseEntry);
         i += 11;
-        document.getElementById("list").innerHTML += '<li id="data_' + c + '" onclick="removeCourse(this)">' + c + '</li>' + "\n";
+        addNewListElement(courseEntry);
     }
     checkRequirements();
 }
-
